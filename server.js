91b4e78afaa79f3317069c730bee302836cbfb86
bayan <bayana.mhmd99@gmail.com>
2020-02-13 23:48:22 +0200

    working

--- server.js
+++
@@ -1,7 +0,0 @@
-users.authenticateBasic(user, password)
-    .then(validUser => {
-      req.token = users.generateToken(validUser);
-      console.log('token:', req.token);
-      next();
-    }).catch( err => next('invalid login'));
-}
